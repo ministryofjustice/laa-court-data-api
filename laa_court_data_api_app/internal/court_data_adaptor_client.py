91910807d8f6c7f0065bb266b7a5021b72e26d37
Paul King <paul.king@digital.justice.gov.uk>
2022-02-04 19:55:32 +0000

    Initial HTTP Clients implementation

---
+++ laa_court_data_api_app/internal/court_data_adaptor_client.py
@@ -0,0 +1,43 @@
+import logging
+from typing import Optional
+import httpx
+from ..config import court_data_adaptor as cda
+from ..internal.oauth_client import OauthClient
+
+logger = logging.getLogger(__name__)
+
+
+async def get(endpoint: str, params: Optional[dict[str, str]] = None, headers: Optional[dict[str, str]] = None):
+    response = await __send_request(method='GET', endpoint=endpoint, params=params, headers=headers)
+    return response
+
+
+async def post(endpoint: str, params: Optional[dict[str, str]] = None, headers: Optional[dict[str, str]] = None,
+               body: Optional[any] = None):
+    response = await __send_request(method='POST', endpoint=endpoint, params=params, headers=headers, body=body)
+    return response
+
+
+async def patch(endpoint: str, params: Optional[dict[str, str]] = None, headers: Optional[dict[str, str]] = None,
+                body: Optional[any] = None):
+    response = await __send_request(method='PATCH', endpoint=endpoint, params=params, headers=headers, body=body)
+    return response
+
+
+async def __send_request(method: str, endpoint: str, params: Optional[dict[str, str]] = None,
+                         headers: Optional[dict[str, str]] = None, body: Optional[any] = None):
+    auth_token = await OauthClient.retrieve_token()
+    if auth_token is None:
+        return auth_token
+
+    async with httpx.AsyncClient(base_url=cda.get_cda_settings().base_url,
+                                 headers=OauthClient.generate_auth_header(auth_token)) as client:
+        try:
+            request = client.build_request(method=method, url=endpoint, params=params, headers=headers, data=body)
+            logger.info("Request_Made")
+            response = await client.send(request)
+            logger.info("Response_Returned", extra={'url': request.url, 'status': response.status_code})
+            return response
+        except:
+            logger.error("Get_Endpoint_Error", extra={'url': request.url})
+            return None
---
+++ test/internal/oauth_client_test.py
@@ -0,0 +1,21 @@
+import httpx
+import respx
+from laa_court_data_api_app.config.court_data_adaptor import CdaSettings
+from laa_court_data_api_app.internal import oauth_client
+
+
+@respx.mock(base_url="http://test-url/")
+async def test_oauth_client_stores_value(respx_mock):
+    respx_mock.post("/oauth/token").mock(return_value=httpx.Response(status_code=200,
+                                                                     json={"access_token": "12345",
+                                                                           "token_type": "Bearer",
+                                                                           "expires_in": "300",
+                                                                           "created_at": "1643981187"}))
+
+    response = await oauth_client.retrieve_token(settings=get_cda_env_vars())
+
+    assert response.access_token == "12345"
+
+
+def get_cda_env_vars():
+    return CdaSettings(cda_endpoint="http://test-url/", cda_uid="12345", cda_secret="123454321")
