2492ac3779667c93d68e911dd79cb1430e68915a
Paul King <paul.king@digital.justice.gov.uk>
2022-02-08 16:29:16 +0000

    Cleanup fixtures

--- test/internal/court_data_adaptor_client_test.py
+++ test/internal/court_data_adaptor_client_test.py
@@ -1,38 +1,23 @@
-from unittest.mock import Mock
-
 from laa_court_data_api_app.internal import court_data_adaptor_client
 from test.internal.internal_fixtures import *
 
 
-async def get_token():
-    return TokenResponse(access_token="12345", token_type="Bearer", expires_in=300, created_at=1643981187)
-
-
-async def test_get_request_returns_correctly(mock_cda_client, get_cda_env_vars):
-    oauth_client = Mock()
-    oauth_client.retrieve_token.side_effect = get_token
-
-    response = await court_data_adaptor_client.get("/get/", oauth_client=oauth_client, settings=get_cda_env_vars)
+async def test_get_request_returns_correctly(mock_cda_client, mock_oauth_client, get_cda_env_vars):
+    response = await court_data_adaptor_client.get("/get/", oauth_client=mock_oauth_client, settings=get_cda_env_vars)
 
     assert response.status_code == 200
     assert response.json() == []
 
 
-async def test_post_request_returns_correctly(mock_cda_client, get_cda_env_vars):
-    oauth_client = Mock()
-    oauth_client.retrieve_token.side_effect = get_token
-
-    response = await court_data_adaptor_client.post("/post/", oauth_client=oauth_client, settings=get_cda_env_vars)
+async def test_post_request_returns_correctly(mock_cda_client, mock_oauth_client, get_cda_env_vars):
+    response = await court_data_adaptor_client.post("/post/", oauth_client=mock_oauth_client, settings=get_cda_env_vars)
 
     assert response.status_code == 200
     assert response.json() == []
 
 
-async def test_patch_request_returns_correctly(mock_cda_client, get_cda_env_vars):
-    oauth_client = Mock()
-    oauth_client.retrieve_token.side_effect = get_token
-
-    response = await court_data_adaptor_client.patch("/patch/", oauth_client=oauth_client, settings=get_cda_env_vars)
+async def test_patch_request_returns_correctly(mock_cda_client, mock_oauth_client, get_cda_env_vars):
+    response = await court_data_adaptor_client.patch("/patch/", oauth_client=mock_oauth_client, settings=get_cda_env_vars)
 
     assert response.status_code == 200
     assert response.json() == {}
--- test/internal/internal_fixtures.py
+++ test/internal/internal_fixtures.py
@@ -1,5 +1,6 @@
 import datetime as dt
-import uuid
+from unittest.mock import Mock
+
 import pytest
 import respx
 
@@ -28,8 +29,12 @@ def get_new_token_response():
                          created_at=int(created_at.total_seconds()))
 
 
+async def get_token_async():
+    return TokenResponse(access_token="12345", token_type="Bearer", expires_in=300, created_at=1643981187)
+
+
 @pytest.fixture(scope="module")
-def mock_cda_client():
+def mock_cda_client(get_new_token_response):
     with respx.mock(base_url="http://test-url/", assert_all_called=False) as respx_mock:
         get_route = respx_mock.get("/get/", name="get_endpoint")
         get_route.return_value = Response(200, json=[])
@@ -38,3 +43,10 @@ def mock_cda_client():
         patch_route = respx_mock.patch("/patch/", name="patch_endpoint")
         patch_route.return_value = Response(200, json={})
         yield respx_mock
+
+
+@pytest.fixture(scope="function")
+def mock_oauth_client():
+    oauth_client = Mock()
+    oauth_client.retrieve_token.side_effect = get_token_async
+    return oauth_client
