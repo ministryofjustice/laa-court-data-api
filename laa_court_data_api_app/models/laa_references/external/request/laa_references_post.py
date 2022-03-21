5e6bdf78550232be91a2bdc1ee83d901c0a770d9
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2022-03-21 09:01:32 +0000

    Add laa_references POST models
    Add internal/request models LaaReferencesPostRequest and LaaReferencesPost for CDA requests.
    
    Add external/request models LaaReferencesPostRequest and LaaReferencesPost.
    Update LaaReferencesPost with an example request body for documentation purposes.

---
+++ laa_court_data_api_app/models/laa_references/external/request/laa_references_post.py
@@ -0,0 +1,18 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class LaaReferencesPost(BaseModel):
+    user_name: str | None
+    defendant_id: UUID | None
+    maat_reference: int | None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "user_name": "jon-5",
+                "defendant_id": "d7f509e8-309c-4262-a41d-ebbb44deab9e",
+                "maat_reference": 1234567,
+            }
+        }
---
+++ laa_court_data_api_app/models/laa_references/internal/request/laa_references_post_request.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+from laa_court_data_api_app.models.laa_references.internal.request.laa_references_post import LaaReferencesPost
+
+
+class LaaReferencesPostRequest(BaseModel):
+    laa_reference: LaaReferencesPost | None
