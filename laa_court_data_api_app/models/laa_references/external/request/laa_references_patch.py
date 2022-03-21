483009658b1f238a82064acf0fec4760257fb0ca
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2022-03-21 09:07:15 +0000

    Update laa_reference PATCH models
    Add example request body to be used in the swagger documentation
    
    Remove unused import

--- laa_court_data_api_app/models/laa_references/external/request/laa_references_patch.py
+++ laa_court_data_api_app/models/laa_references/external/request/laa_references_patch.py
@@ -8,3 +8,13 @@ class LaaReferencesPatch(BaseModel):
     defendant_id: UUID | None
     maat_reference: int | None
     unlink_reason_code: int | None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "user_name": "jon-5",
+                "defendant_id": "d7f509e8-309c-4262-a41d-ebbb44deab9e",
+                "maat_reference": 1234567,
+                "unlink_reason_code": 0
+            }
+        }
