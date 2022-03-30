96fd6d65cd6830b82699da68a8b581ff3a5e523f
Paul King <84575729+pking-moj@users.noreply.github.com>
2022-03-30 11:36:42 +0100

    AAC-557 - Update pipeline for UAT and Staging (#49)
    * Update pipeline for UAT and Staging
    
    * Fix pipeline config
    
    * fix values for ingress
    
    * adjust ingress
    
    * Update UAT test vars
    
    * remove basic auth
    
    * revert changes
    
    * Rerun Pipeline
    
    * fix endpoints
    
    * Fix bad property based on UAT testing
    
    * Update main pipeline and name test steps

--- laa_court_data_api_app/models/hearing/hearing_result.py
+++ laa_court_data_api_app/models/hearing/hearing_result.py
@@ -8,4 +8,4 @@ from laa_court_data_api_app.models.hearing.hearing import Hearing
 
 class HearingResult(BaseModel):
     hearing: Optional[Hearing] = None
-    shared_time: Optional[datetime] = None
+    shared_time: Optional[str] = None
