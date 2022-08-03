c0221c0b9d7124cb3ee25e9b3b029500dcfadb0a
Paul King <paul.king@digital.justice.gov.uk>
2022-08-03 14:39:59 +0100

    Fix ping endpoint

--- laa_court_data_api_app/routers/ping.py
+++ laa_court_data_api_app/routers/ping.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from laa_court_data_api_app.config.app import AppSettings, get_app_settings
+from laa_court_data_api_app.config.secure_headers import SecureJsonResponse
 from laa_court_data_api_app.models.ping import Ping
 
 router = APIRouter()
@@ -7,8 +8,11 @@ router = APIRouter()
 
 @router.get('/ping', response_model=Ping)
 async def ping(settings: AppSettings = Depends(get_app_settings)):
-    results = {'app_branch': settings.app_branch,
-               'build_date': settings.build_date,
-               'build_tag': settings.build_tag,
-               'commit_id': settings.commit_id}
-    return results
+    results = Ping(
+        app_branch=settings.app_branch,
+        build_date=settings.build_date,
+        build_tag=settings.build_tag,
+        commit_id=settings.commit_id
+        )
+
+    return SecureJsonResponse(status_code=200, content=results)
